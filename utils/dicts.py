682db76b34bf3a5de2797bbe3acfd5a54e559f8c
malpoly <marry2691@yandex.ru>
2023-02-25 14:38:48 +0300

    made dicts.py and test_dicts.py with 100%

--- utils/dicts.py
+++ utils/dicts.py
@@ -1,2 +1,24 @@
-def get_val(collection, key, default='git'):
-    pass
+def get_val(collection, keys, default='git'):
+    """
+        Извлекает из словаря по переданному ключу, если ключ существует.
+        Если ключ не существует, возвращает значение по умолчанию.
+        Функция работает только с неотрицательными индексами.
+        :param collection: исходный словарь.
+        :param key: ключ извлекаемого элемента.
+        :param default: значение по-умолчанию.
+        :return: значение по ключу ли значение по-умолчанию.
+        """
+    default = 'git' if default is None else default
+
+    for key, item in collection.items():
+        if key == keys:
+            return item
+        else:
+            return default
+
+    length = len(collection)
+    if length == 0:
+        return default
+
+
+
